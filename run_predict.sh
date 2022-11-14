afc4fd7d6017019734c397433f6f01107578b013
Tswings <39293946+Tswings@users.noreply.github.com>
2022-11-14 17:46:55 +1300

    Update run_predict.sh

--- run_predict.sh
+++ run_predict.sh
@@ -17,7 +17,7 @@ cd QA
 echo "Question Answering:"
 CUDA_VISIBLE_DEVICES=0,5 python run_qa.py --bert_model albert-xxlarge-v2 --checkpoint_dir qa_model --output_dir qa_results --predict_file pred.json --test_supporting_para_file ../PS2/ps2_result/dev_related.json --model_name AlbertForQuestionAnswering --log_prefix predict_log --overwrite_result True --test_file ../data/input.json --local_rank -1 --val_batch_size 32 --no_network True
 
-echo "Good"
+echo "Finished."
 
 
 
